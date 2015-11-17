f6aaccdc291761f87d183e5cb098495ad4d3ad81
Zhongxi Wang <Ben_Big@Big.local>
2015-11-16 21:08:18 -0500

    Start using Angular Route

---
+++ client/config.js
@@ -0,0 +1,26 @@
+(function(){
+  angular
+    .module("BlogApp")
+    .config(Configuration)
+
+
+    function Configuration($routeProvider)
+    {
+      $routeProvider
+        .when('/home',{
+          templateUrl: './view/Home/Home.view.html'
+        })
+        .when('/upload',{
+          templateUrl: './view/Upload/Upload.view.html'
+        })
+        .when('/contact',{
+          templateUrl: './view/Contact/Contact.view.html'
+        })
+        .when('/individual',{
+          templateUrl: './view/Individual/Individual.html'
+        })
+        .otherwise({
+            redirectTo:'/home'
+        })
+    }
+})();

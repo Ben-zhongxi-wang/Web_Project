33dde22718e93776115630adf70dc274853960e1
Ben <ben@small.local>
2015-11-23 00:03:36 -0500

    Change navigation.view a bit

--- client/view/Navigation/Navigation.controller.js
+++ client/view/Navigation/Navigation.controller.js
@@ -6,6 +6,7 @@
 
 
 function navigationController($http,$rootScope,$scope,$location){
+		$scope.currentUser=$rootScope.currentUser;
 		$scope.searching=function(){
 			$location.path("/home").search('searchText',$scope.searchText);
 		}
